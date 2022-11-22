e3d051b9ccf3d42c2a5859132f78ebfc0b3879ed
Dastan T <dastan.turatbekov@gmail.com>
2022-11-22 18:41:58 +0600

    обновил admin.py и сделал миграцию

--- source/task_tracker/admin.py
+++ source/task_tracker/admin.py
@@ -3,26 +3,12 @@ from task_tracker.models import Task, Status, Type
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'summary', 'status', 'type', 'created_at']
-    list_filter = ['status', 'type']
+    list_display = ['id', 'summary', 'status', 'created_at']
+    list_filter = ['status']
     search_fields = ['summary']
     exclude = []
 
 
-class StatusAdmin(admin.ModelAdmin):
-    list_display = ['id', 'status_name']
-    list_filter = ['status_name']
-    search_fields = ['status_name']
-    exclude = []
-
-
-class TypeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'type_name']
-    list_filter = ['type_name']
-    search_fields = ['type_name']
-    exclude = []
-
-
 admin.site.register(Task, TaskAdmin)
-admin.site.register(Type, TypeAdmin)
-admin.site.register(Status, StatusAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
