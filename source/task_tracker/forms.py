9187beb7b53f7283060ba9eb623749654c5e923a
Dastan T <dastan.turatbekov@gmail.com>
2022-11-24 18:11:02 +0600

    Добавил валидацию в формах на зарпешенные слова для summary

--- source/task_tracker/forms.py
+++ source/task_tracker/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import widgets
-from task_tracker.models import Type, Status, Task
+from task_tracker.models import Task
+from django.core.validators import ValidationError
 
 
 class TaskForm(forms.ModelForm):
@@ -9,3 +10,12 @@ class TaskForm(forms.ModelForm):
         fields = ['summary', 'description', 'status', 'types']
         widgets = {'description': widgets.Textarea(attrs={"cols": 24, "rows": 3}),
                    'types': widgets.CheckboxSelectMultiple}
+
+    def clean_summary(self):
+        banned_words = ['sex', 'bomb', 'kill', 'porn', 'секс', 'бомба', 'убью', 'порно']
+        summary = self.cleaned_data.get('summary')
+        summary_lst = summary.split()
+        for word in summary_lst:
+            if word in banned_words:
+                raise ValidationError("Пожалуйста, не используйте запрещенные слова")
+        return summary
