07aa4140efb45f17770fe39f5ff7de78fbe42b38
Dastan T <dastan.turatbekov@gmail.com>
2022-11-24 17:57:41 +0600

    Добавил валидатор по минимальному значению для описания(>20)

--- source/task_tracker/models.py
+++ source/task_tracker/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Status(models.Model):
@@ -17,7 +18,8 @@ class Type(models.Model):
 
 class Task(models.Model):
     summary = models.CharField(max_length=40, null=False, blank=False, verbose_name='заголовок')
-    description = models.TextField(max_length=500, null=True, blank=True, verbose_name='описание')
+    description = models.TextField(max_length=500, null=True, blank=True, verbose_name='описание',
+                                   validators=[MinLengthValidator(20),])
     status = models.ForeignKey(
         'task_tracker.Status', related_name='statuses',
         on_delete=models.RESTRICT, verbose_name='статус')
