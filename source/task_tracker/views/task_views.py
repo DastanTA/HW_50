feba6b4c897b4bbe2305fb4982a414cb605b90ec
Dastan T <dastan.turatbekov@gmail.com>
2022-12-08 22:51:50 +0600

    HW-55: Обновление задачи переписан на UpdateView

--- source/task_tracker/views/task_views.py
+++ source/task_tracker/views/task_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
-from django.views.generic import View, TemplateView, FormView, CreateView
+from django.views.generic import View, TemplateView, FormView, CreateView, UpdateView, DeleteView
 from django.db.models import Q
 from task_tracker.models import Task, Project
 from task_tracker.forms import TaskForm, ProjectTaskForm
@@ -54,34 +54,14 @@ class ProjectTaskCreateView(CreateView):
 
 
 
-class UpdateTask(FormView):
+class UpdateTask(UpdateView):
     template_name = 'tasks/update.html'
+    model = Task
     form_class = TaskForm
-
-    def get_object(self):
-        pk = self.kwargs.get('pk')
-        return get_object_or_404(Task, pk=pk)
-
-    def dispatch(self, request, *args, **kwargs):
-        self.task = self.get_object()
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = self.task
-        return context
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['instance'] = self.task
-        return kwargs
-
-    def form_valid(self, form):
-        self.task = form.save()
-        return super().form_valid(form)
+    context_object_name = 'task'
 
     def get_success_url(self):
-        return reverse('view_task', kwargs={'pk': self.task.pk})
+        return reverse('view_task', kwargs={'pk': self.object.pk})
 
 
 class DeleteTask(View):
