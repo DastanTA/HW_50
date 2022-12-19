046a0ba27b73e5c8876d6e9d8bcc6c5126ee9ca5
Dastan T <dastan.turatbekov@gmail.com>
2022-12-19 21:59:05 +0600

    HW - 58: представление и шаблон с формой созданы

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,19 +1,15 @@
-# from django.shortcuts import render, redirect
-# from django.contrib.auth import authenticate, login, logout
-#
-# def login_view(request):
-#     context = {}
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect('task_tracker:project_main')
-#         else:
-#             context['has_error'] = True
-#     return render(request, 'registration/login.html', context=context)
-#
-# def logout_view(request):
-#     logout(request)
-#     return redirect('task_tracker:project_main')
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from accounts.forms import UserCreationForm
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_tracker:project_main')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/user_create.html', context={'form': form})
