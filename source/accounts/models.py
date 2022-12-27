4a6e4f2c160b7fef3c0b0d56b54cef4d742bc4c7
Dastan T <dastan.turatbekov@gmail.com>
2022-12-27 14:20:14 +0600

    HW-60 - создал модель. Settings и Админка соответсвтенно настроены

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE,
+                                verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    git_hub_profile = models.URLField(max_length=300, null=True, blank=True, verbose_name='Профиль на GitHub')
+    about = models.TextField(max_length=600, null=True, blank=True, verbose_name='О себе')
+
+    def __str__(self):
+        return self.user.first_name + "'s Profile"
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
