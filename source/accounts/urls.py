e495d4f576021c50a76c007e1d318a15edc8a2f5
Dastan T <dastan.turatbekov@gmail.com>
2023-01-06 15:45:23 +0600

    HW-61: Редактирование провиля реализовано. Шаблоны сделаны, в url зарегистрировано

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accounts.views import RegisterView, UserDetailView, AllUsersView
+from accounts.views import RegisterView, UserDetailView, AllUsersView, UserChangeView
 
 
 app_name = 'accounts'
@@ -10,5 +10,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('<int:pk>/', UserDetailView.as_view(), name='detail'),
+    path('<int:pk>/change/', UserChangeView.as_view(), name='change'),
     path('all_users/', AllUsersView.as_view(), name='all_users'),
 ]
